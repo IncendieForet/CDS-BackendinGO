a0f7e3ea66a5b37e0fc0f6e720d47cb8be1044c5
Adam02 <80120070+IncendieForet@users.noreply.github.com>
2024-12-05 18:21:36 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"strings"
+	"time"
+
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
@@ -32,13 +36,15 @@ func main() {
 		})
 	})
 	r.GET("/jobs/:id", getJobByID)
+	r.GET("/jobs/skills", getAllParsedJobSkills)
 	r.Run()
 }
 
 // Our implementation logic for connecting to MongoDB
 func connect_to_mongodb() error {
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI).SetCompressors([]string{"zstd"})
+
 	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
 		panic(err)
@@ -64,6 +70,81 @@ func getJobByID(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	// Extract the job skill field
+	jobSkill, ok := job["job_skills"]
+	if !ok {
+		c.JSON(http.StatusNotFound, gin.H{"error": "job_skill not found"})
+		return
+	}
 	// Return job
-	c.JSON(http.StatusOK, job)
+	c.JSON(http.StatusOK, gin.H{"job_skill": jobSkill})
+}
+
+func getAllParsedJobSkills(c *gin.Context) {
+	fmt.Printf("Yeah")
+	//get exact microsecond for performance measurement (measured in nanoseconds)
+
+	start := time.Now()
+
+	var err error
+	var cursor *mongo.Cursor
+	// Define a cursor to iterate over the collection
+	cursor, err = mongoClient.Database("linkedin").Collection("jobs").Find(
+		context.TODO(),
+		bson.D{},
+		options.Find().SetLimit(40000).SetProjection(bson.D{{"job_skills", 1}}),
+	)
+	
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer cursor.Close(context.TODO())
+
+	// Map to store unique skills
+	skillSet := make(map[string]bool)
+	var job bson.M
+	var ok bool
+	var skillsStr string
+	//empty array of strings
+	var skills = []string{}
+	var skill string
+
+	fmt.Printf("Time taken for request in microseconds: %v\n", time.Since(start).Microseconds())
+	start = time.Now()
+
+	// Iterate through the cursor
+	for cursor.Next(context.TODO()) {
+		cursor.Decode(&job)
+		// Extract job_skill field
+		if skillsStr, ok = job["job_skills"].(string); ok {
+			// Split the string into individual skills
+			skills = strings.Split(skillsStr, ", ")
+			for _, skill = range skills {
+				//check if skill is already in the map
+				if !skillSet[skill] {
+					skillSet[skill] = true
+					//jobSkills = append(jobSkills, skill)
+				}
+			}
+		}
+	}
+	fmt.Printf("Time taken for processing in microseconds: %v\n", time.Since(start).Microseconds())
+	start = time.Now()
+	//convert map to array
+	var jobSkills []string = make([]string, 0, len(skillSet))
+	for i := range skillSet {
+		jobSkills = append(jobSkills, i)
+	}
+
+	fmt.Printf("Time taken for converting in microseconds: %v\n", time.Since(start).Microseconds())
+
+	// Check if we got any skills
+	if len(jobSkills) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"message": "No job skills found"})
+		return
+	}
+
+	// Return parsed skills
+	c.JSON(http.StatusOK, gin.H{"job_skills": jobSkills})
 }
